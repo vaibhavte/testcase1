c868477bd6db82060f214e731120585dab959433
ews <ews@DESKTOP-II6E4HT>
2022-07-29 18:05:46 +0530

    second commit

---
+++ testcasete/src/testcasete/DuplicateArray.java
@@ -0,0 +1,17 @@
+package testcasete;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateArray {
+	public static void main(String[] args) {
+		int []a= {20,45,14,20,45,1,3,5,55,3};
+		Set<Integer>s=new HashSet<Integer>();
+		for(int num:a) {
+			if(s.add(num)==false) {
+				System.out.println(num);
+			}
+		}
+		
+	}
+}
